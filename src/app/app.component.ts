d6a092442c5e153e337a0698a98e079d71e86e31
Pei <jzha471@aucklanduni.ac.nz>
2017-10-23 13:41:53 +1300

    195. Reactive:Creating Customer Validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,13 @@ import {FormArray, FormControl, FormGroup, Validator, Validators} from '@angular
 export class AppComponent implements OnInit{
   genders = ['male', 'female'];
   signupForm : FormGroup ;
+    forbiddenUserNames = [' ', 'Anna'];
+
 
   ngOnInit(){
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email]) ,
       }),
 
@@ -29,5 +31,12 @@ export class AppComponent implements OnInit{
   onAddHobby(){
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get('hobbies')).push(control) ;
+  }
+
+  forbiddenNames( control: FormControl ) :{[s: string]:boolean}{
+    if(this.forbiddenUserNames.indexOf(control.value) !== -1 ){
+      return {'nameIsForbidden':true};
+    }
+    return null ;
   }
 }
