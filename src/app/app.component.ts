71f8ecf38c1eeb0255594ffda6ea7e6004da6873
Pei <jzha471@aucklanduni.ac.nz>
2017-10-23 14:31:28 +1300

    199. Reactive:Setting and Patching Values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent implements OnInit{
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email],this.forbiddenEmails) ,
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails) ,
       }),
 
       'gender': new FormControl('male'),
@@ -33,13 +33,30 @@ export class AppComponent implements OnInit{
     this.signupForm.statusChanges.subscribe(
       (status) => {
         console.log(status) ;
+      });
+
+    // this.signupForm.setValue({
+    //   'userData': {
+    //     'username': 'Max',
+    //     'email': 'max@test.com'
+    //   },
+    //   'gender': 'male',
+    //   'hobbies': []
+    //
+    // });
+
+    this.signupForm.patchValue({
+      'userData': {
+        'username': 'Anna',
+        'email': 'max@test.com'
       }
-    );
+    });
 
   }
 
   onSubmit(){
     console.log(this.signupForm) ;
+    this.signupForm.reset() ;
   }
 
   onAddHobby(){
@@ -47,23 +64,23 @@ export class AppComponent implements OnInit{
     (<FormArray>this.signupForm.get('hobbies')).push(control) ;
   }
 
-  forbiddenNames( control: FormControl ) :{[s: string]: boolean}{
-    if(this.forbiddenUserNames.indexOf(control.value) !== -1 ){
-      return {'nameIsForbidden':true};
+  forbiddenNames( control: FormControl ) : {[s: string]: boolean}{
+    if (this.forbiddenUserNames.indexOf(control.value) !== -1 ){
+      return {'nameIsForbidden': true};
     }
     return null ;
   }
 
   forbiddenEmails(control: FormControl ): Promise<any>| Observable<any>{
-    const promise = new Promise<any>((resolve, reject) =>{
-     setTimeout(() =>{
-        if( control.value === 'test@test.com'){
+    const promise = new Promise<any>((resolve, reject) => {
+     setTimeout(() => {
+        if ( control.value === 'test@test.com'){
           resolve({'emailIsForbidden': true});
         }else
         {
           resolve(null) ;
         }
-     },1500);
+     }, 1500);
     }) ;
     return promise ;
   }
